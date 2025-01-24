ad6a4196a27998c24745c06636069c5c36b2b4d9
santaETH <sdamissie@yahoo.com>
2025-01-24 15:56:03 +0300

    API fetching users

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -4,10 +4,10 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 // User Routes
-router.post('/users', userController.createUser); // Create a new user
-router.get('/users', userController.getAllUsers); // Get all users
-router.get('/users/:email', userController.getUserByEmail); // Get user by email
-router.put('/users/:email', userController.updateUser); // Update user by email
-router.delete('/users/:email', userController.deleteUser); // Delete user by email
+router.post('/', userController.createUser); // Create a new user
+router.get('/', userController.getAllUsers); // Get all users
+router.get('/:email', userController.getUserByEmail); // Get user by email
+router.put('/:email', userController.updateUser); // Update user by email
+router.delete('/:email', userController.deleteUser); // Delete user by email
 
 module.exports = router;
